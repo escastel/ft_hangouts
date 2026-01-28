97977c0580c1da6aeaac2cd5380e83c3583a54f5
Ester Castellano Ríos <estercastellanorios@gmail.com>
2026-01-28 18:51:45 +0100

    feat: add contact model

---
+++ app/src/main/java/com/example/ft_hangouts/data/models/Contact.kt
@@ -0,0 +1,11 @@
+package com.example.ft_hangouts.data.models
+
+data class Contact(
+    val id: Long = 0,
+    val name: String,
+    val phonePrefix: String = "",
+    val phoneNumber: String = "",
+    val address: String = "",
+    val notes: String = "",
+    val createdAt: Long = System.currentTimeMillis()
+) {}
